83897d8546510bbc43c75e131779cc91a017f352
mxber2022 <mxber2022@gmail.com>
2025-02-08 20:12:51 +0100

    Working modal for zkverify

--- frontend/src/contexts/AccountContext.tsx
+++ frontend/src/contexts/AccountContext.tsx
@@ -1,26 +1,44 @@
-import React, { createContext, useContext, useState, ReactNode, FC } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  FC,
+} from "react";
 
 interface AccountContextType {
-    selectedAccount: string | null;
-    setSelectedAccount: React.Dispatch<React.SetStateAction<string | null>>;
+  selectedAccount: string | null;
+  selectedWallet: string | null;
+  setSelectedAccount: React.Dispatch<React.SetStateAction<string | null>>;
+  setSelectedWallet: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
-export const AccountContext = createContext<AccountContextType | undefined>(undefined);
+export const AccountContext = createContext<AccountContextType | undefined>(
+  undefined
+);
 
 export const AccountProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const [selectedAccount, setSelectedAccount] = useState<string | null>(null);
+  const [selectedAccount, setSelectedAccount] = useState<string | null>(null);
+  const [selectedWallet, setSelectedWallet] = useState<string | null>(null);
 
-    return (
-        <AccountContext.Provider value={{ selectedAccount, setSelectedAccount }}>
-            {children}
-        </AccountContext.Provider>
-    );
+  console.log("selectedWallet: hehe ", selectedWallet);
+  return (
+    <AccountContext.Provider
+      value={{
+        selectedAccount,
+        setSelectedAccount,
+        selectedWallet,
+        setSelectedWallet,
+      }}
+    >
+      {children}
+    </AccountContext.Provider>
+  );
 };
 
 export const useAccount = () => {
-    const context = useContext(AccountContext);
-    if (!context) {
-        throw new Error('useAccount must be used within an AccountProvider');
-    }
-    return context;
+  const context = useContext(AccountContext);
+  if (!context)
+    throw new Error("useAccount must be used within an AccountProvider");
+  return context;
 };
